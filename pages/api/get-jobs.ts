6e681cb4eeefcf0619c57fa0e10d44f9a550328a
Khubaib Qaiser <khubaib.qaiser@achieve-test-prep.com>
2024-01-24 09:41:58 -0500

    Filter published jobs only

--- pages/api/get-jobs.ts
+++ pages/api/get-jobs.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { apiClient } from "../../utils/api-client";
+import { Job } from "../../types/job";
 
 export default async function handler(
   req: NextApiRequest,
@@ -11,7 +12,7 @@ export default async function handler(
     const { page, per_page } = req.query as { page: string; per_page: string };
 
     // Make a GET request to the Zoho API to retrieve job openings
-    const response = await apiClient.request<{ data: unknown }>({
+    const response = await apiClient.request<{ data: Job[] }>({
       baseUrl: "https://recruit.zoho.com/recruit/v2",
       endpoint: "/JobOpenings",
       params: {
@@ -23,9 +24,7 @@ export default async function handler(
     // Extract the job openings from the response
     const jobOpenings = response.data;
 
-    console.log("CHECK - JOBS", response);
-
-    res.status(200).json(jobOpenings);
+    res.status(200).json(jobOpenings.filter((job) => !!job.Publish));
   } catch (error) {
     res.status(500).json({ error });
   }
